72143448806b1377ccb50bf5329dfb9ec5a46815
samueldj <samueldj@hawaii.edu>
2014-04-03 22:17:42 -1000

    Update Assignment6.lisp
    Starting off with Assignment5.lisp to do Assignment6.lisp

--- Assignment6.lisp
+++ Assignment6.lisp
@@ -1,1 +1,281 @@
-h
+;;; -*- Mode: LISP; Syntax: Common-lisp; Package: USER; Base: 10 -*-
+;;; Name: Samuel Kim, Edgar Lau, Cyrus Wu         Date:03/18/14
+;;; Course: ICS313        Assignment: 5   
+;;; File: samueldj5.lisp
+
+; This parameter shows the description to each place.
+(defparameter *nodes* '((living-room (you are in the living-room.
+                            a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                            there is a well in front of you.))
+                        (attic (you are in the attic.
+                            there is a giant welding torch in the corner.))
+                        (kitchen (you are in the kitchen. 
+                            There is a platter in front of you.))))
+
+; This function describes the location.
+(defun describe-location (location nodes)
+   (cadr (assoc location nodes)))
+
+; This parameter contains the paths that players can take to move between places.
+(defparameter *edges* '((living-room (garden west door)  
+                                     (attic upstairs ladder)
+                                     (kitchen north door))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))
+                        (kitchen (living-room south door))))
+
+; This function describes a specific direction of a path from one location.
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+; This function describes a locations paths going into and out of it.
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+; This parameter defines the object to this game.
+(defparameter *objects* '(whiskey bucket starpiece1 frog starpiece2 chain cake starpiece3))
+
+; This parameter gives where the objects are in the game.
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (starpiece1 living-room)
+                                   (chain garden)
+                                   (frog garden)
+                                   (starpiece2 garden)
+                                   (cake kitchen)
+                                   (starpiece3 kitchen)
+                                   (starpiece4 attic)))
+
+; This function lists all the object to a specific location.
+(defun objects-at (loc objs obj-loc)
+   (labels ((is-at (obj)
+              (eq (cadr (assoc obj obj-loc)) loc)))
+       (remove-if-not #'is-at objs)))
+
+; This function describes the objects in a specific location.
+(defun describe-objects (loc objs obj-loc)
+   (labels ((describe-obj (obj)
+                `(you see a ,obj on the floor.)))
+      (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+
+; This parameter gives the starting location for this game.
+(defparameter *location* 'living-room)
+
+; This function allows users to see where they are in this wizard's world.
+(defun look ()
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
+
+; This function allow users to go to different locations.
+(defun walk (direction)
+  (labels ((correct-way (edge)
+             (eq (cadr edge) direction)))
+    (let ((next (find-if #'correct-way (cdr (assoc *location* *edges*)))))
+      (if next 
+          (progn (setf *location* (car next)) 
+                 (look))
+          '(you cannot go that way.)))))
+
+; This function allow users to pickup something in a location.
+(defun pickup (object)
+  (cond ((member object (objects-at *location* *objects* *object-locations*))
+         (push (list object 'body) *object-locations*)
+         `(you are now carrying the ,object))
+	  (t '(you cannot get that.))))
+
+; This function shows all of the objects that you picked up.
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+; This function shows if you have a specific object in your inventory.                          
+(defun have (object)
+    (member object (cdr (inventory))))
+    
+; This function prints all available commands in the Wizard's World.                                   
+(defun help ()
+  `(you can use one of the following commands- ,@*allowed-commands*))
+
+; Alias to help, just lets user call help with "h" instead                  
+(defun h ()
+  (help))
+
+; Alias to help, just lets user call help with "?" instead                  
+(defun ? ()
+  (help))
+
+
+; This is a function from Dr. Reed's samples
+(defun our-member (obj lst)
+  (if (null lst)
+      nil
+      (if (eql (caar lst) obj)
+          t
+          (our-member obj (cdr lst)))))
+
+
+; This is a function from Dr. Reed's samples
+; modified for objects
+(defun our-member-obj (obj lst)
+  (if (null lst)
+      nil
+      (if (eql (car lst) obj)
+          t
+          (our-member-obj obj (cdr lst)))))
+
+
+;;;=========================R-E-P-L=======================;;;
+;  Wizard's World part 2
+
+; This function allows the user to play the game. If user types quit, the game is over.
+(defun game-repl ()
+    (let ((cmd (game-read)))
+        (unless (eq (car cmd) 'quit)
+            (game-print (game-eval cmd))
+            (game-repl))))
+
+; This function forces users to put parentheses around their commands and
+; quotes in front of any function commands.
+(defun game-read ()
+    (let ((cmd (read-from-string (concatenate 'string "(" (read-line) ")"))))
+         (flet ((quote-it (x)
+                    (list 'quote x)))
+             (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+; This parameter gives the allowed commands for this game so no other commands will 
+; be allowed for this game.
+(defparameter *allowed-commands* '(look walk pickup inventory help h ?))
+
+; This function evaluates the commands that is okay or not for the user to use.
+(defun game-eval (sexp)
+    (if (member (car sexp) *allowed-commands*)
+        (eval sexp)
+        '(i do not know that command.)))
+
+; This function tweaks the text to this game.
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+          (rest (cdr lst)))
+      (cond ((eql item #\space) (cons item (tweak-text rest caps lit)))
+            ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+            ((eql item #\") (tweak-text rest caps (not lit)))
+            (lit (cons item (tweak-text rest nil lit)))
+            (caps (cons (char-upcase item) (tweak-text rest nil lit)))
+            (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+; This function prints the game in it's correct characters instead of just capitals.
+(defun game-print (lst)
+    (princ (coerce (tweak-text (coerce (string-trim "() " (prin1-to-string lst)) 'list) t nil) 'string))
+    (fresh-line))
+;;;==========================Macros===============================;;;           
+
+; Macro for new objects, need to push to *objects* and                      
+; to the *object-location*                                                  
+(defmacro new-object (object location)
+  `(progn
+     (cond
+       ((and 
+          (not (our-member-obj ',object *objects*))
+          (our-member ',location *nodes*))
+        (pushnew ',object *objects*)
+        (pushnew '(,object ,location) *object-locations*))
+       (t "Object already exists"))))
+
+; test: add new object pillow to living-room                                
+(new-object broom kitchen)
+
+; Main macro for the new locations in game. We need to push a               
+; new node with name and description.                                       
+(defmacro new-location (object &body body)
+  `(cond
+   ((not (our-member ',object *nodes*))
+    (pushnew '(,object (,@body)) *nodes*))
+   (t "Location already exists")))
+
+; Adding a new location in to test                                     
+(new-location bedroom You are now in the bedroom. Every wizard needs his sleep.)
+(new-location outside You have jumped out the window and died. The end.)
+
+; New implementation of new-path macro
+; We only need the following args:
+; origin, destination, direction, path
+; Optional arg: direction-back
+(defmacro new-path (origin destination direction path &optional (direction-back "unable"))
+  `(cond
+    ((or
+      ; case 1:origin doesn't exist                                             
+      (not (our-member ',origin *nodes*))
+      ; case 2:destination doesn't exist                                        
+      (not (our-member ',destination *nodes*)))
+     ()"Missing location, cannot create path.")
+     ; case 3:okay to implement                                                 
+    (t(progn
+        (if (equal ',direction-back "unable")
+            nil
+            ;now we need to test to see if the destination is already there       
+          (cond
+           ((our-member ',destination *edges*)
+            (pushnew '(,origin ,direction-back ,path)
+                 (cdr (assoc ',destination *edges*)) :test 'equal))
+           (t (pushnew '(,destination
+                          (,origin ,direction-back ,path)) *edges* :test 'equal))))
+
+          ; add new location/direction/path to origin's list of edges           
+        (pushnew '(,destination ,direction ,path)
+                 (cdr (assoc ',origin *edges*)) :test 'equal)))))
+
+; Add paths to the new location
+(new-path living-room bedroom east door west)
+(new-path attic outside outside window "unable")
+
+; Macro to run different commands inside the game-repl
+(defmacro game-action (command subj obj place &body body)
+  `(progn (defun ,command (subject object)
+            (if (and (eq *location* ',place)
+                     (eq subject ',subj)
+                     (eq object ',obj)
+                     (have ',subj))
+                ,@body
+            '(i cant ,command like that.)))
+          (pushnew ',command *allowed-commands*)))
+
+; Sets the parameter for the welded chain object
+(defparameter *chain-welded* nil)
+
+; Uses the game-action macro to weld the chain and the bucket.
+(game-action weld chain bucket attic
+             (if (and (have 'bucket) (not *chain-welded*))
+                 (progn (setf *chain-welded* 't)
+                        '(the chain is now securely welded to the bucket.))
+               '(you do not have a bucket.)))
+
+; Sets the combined piece using the pieces from the previous assignment
+(defparameter *star-power* nil)
+
+; Uses the game-action macro to combine the star pieces into one object
+(game-action power starpiece1 starpiece2 attic
+             (if (and (have 'starpiece1) (not *star-power*))
+                 (progn (setf *star-power* 't)
+                        '(the star has been formed and is giving full power.))
+               '(you do not have all the pieces to the star.)))
+
+; Creates the parameter for the filled bucket object
+(defparameter *bucket-filled* nil)
+
+; Uses the game-action macro to fill the bucket with water from the well.
+(game-action dunk bucket well garden
+             (if *chain-welded* 
+                 (progn (setf *bucket-filled* 't)
+                        '(the bucket is now full of water))
+               '(the water level is too low to reach.)))
+
+; Uses the game-action macro to build a new command that determines the outcome of the game.
+(game-action splash bucket wizard living-room
+             (cond ((not *bucket-filled*) '(the bucket has nothing in it.))
+                   ((have 'frog) '(the wizard awakens and sees that you stole his frog. 
+                                   he is so upset he banishes you to the 
+                                   netherworlds- you lose! the end.))
+                   (t '(the wizard awakens from his slumber and greets you warmly. 
+                        he hands you the magic low-carb donut- you win! the end.))))
