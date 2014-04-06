b10e0382cb7535951b092643f1d35033d9c422fc
Edgar Lau <laue09@gmail.com>
2014-04-06 05:38:34 -1000

    add

--- Assignment6.lisp
+++ Assignment6.lisp
@@ -1,45 +1,71 @@
 ;;; -*- Mode: LISP; Syntax: Common-lisp; Package: USER; Base: 10 -*-
-;;; Name: Samuel Kim, Edgar Lau, Cyrus Wu         Date:03/18/14
-;;; Course: ICS313        Assignment: 5   
-;;; File: samueldj5.lisp
+;;; Name: Samuel Kim, Edgar Lau, Cyrus Wu         Date:04/4/14
+;;; Course: ICS313        Assignment: 6   
+;;; File: assignment6.lisp
 
 ; This parameter shows the description to each place.
-(defparameter *nodes* '((living-room (you are in the living-room.
-                            a wizard is snoring loudly on the couch.))
-                        (garden (you are in a beautiful garden.
-                            there is a well in front of you.))
-                        (attic (you are in the attic.
-                            there is a giant welding torch in the corner.))
-                        (kitchen (you are in the kitchen. 
+(defparameter *nodes* '((house (you are currently at your house.
+                            You hear commotion outside.))
+                        (village (you are in a small town.
+                            There is panic among the villagers as monsters have appeared and people are missing!))
+                        (forest-trail (you are in the forest trail.
+                            A worn sign says there is a village to the north, a cave to the south, and a mountain to the west.))
+                        (Castle (you are in the Castle.
+                           The king has offered a reward to someone who can kill the monsters. ))
+                        (catacombs (you are in the catacombs beneath the cathedral.
+                           Skulls align the walls and there is a foul smell in the air.  ))
+                        (dark-cave (you are in a dark cave. 
+                            ))
+                        (labyrinth (You trip and fall down a hole! You are trapped in the labyrinth! It is hard to see. 
+                            ))
+                        (cathedral (you are in a cathedral. People around you are praying for their safety.
                             Default text))
-                        (area5 (you are in the area5 
+                        (mountain (you are on a tall mountain. You hear noises coming from a cave. 
                             Default text))
-                        (area6 (you are in the area6 
-                            Default text))
-                        (area7 (you are in the area7 
-                            Default text))
-                        (area8 (you are in the area8 
-                            Default text))
-                        (area9 (you are in the area9 
-                            Default text))
-                        (area10 (you are in the area10 
-                            Default text))))
+                        (labyrinth2 (You are in a dark corridor.
+                                             ))
+                        (labyrinth3 (You are in a dark corridor.
+                                            ))
+                        (labyrinth4 (You are in a dark corridor.
+                            You hear growls coming from someplace deeper in the maze.))
+                        (hole (You have fell down a hole and died! Game Over.
+                                   ))
+                        (minotaur-lair (A large minotaur roars and charges at you. 
+                                        ))))
 
 ; This function describes the location.
 (defun describe-location (location nodes)
    (cadr (assoc location nodes)))
 
 ; This parameter contains the paths that players can take to move between places.
-(defparameter *edges* '((living-room (garden west door)  
-                                     (attic upstairs ladder)
-                                     (kitchen north door))
-                        (garden (living-room east door))
-                        (attic (living-room downstairs ladder))
-                        (kitchen (living-room south door))
-
-
-
-))
+(defparameter *edges* '((house (village outside door))
+                        (village (Castle west path)
+                                 (house inside door)
+                                 (cathedral east path)
+                                 (forest-trail south path))
+                        (Castle (village east path))
+                        (cathedral (village west path)
+                                   (catacombs down hole))
+                        (forest-trail (village north path)
+                                      (mountain west path)
+                                      (dark-cave south path))
+                        (catacombs (cathedral up hole))
+                        (mountain (forest-trail east path))
+                        (dark-cave (forest-trail north path)
+                                   (labyrinth south broken-door))
+                        (labyrinth (labyrinth2 east path)
+                                   (hole south path)
+                                   (hole west path))
+                        (labyrinth2 (hole north path)
+                                    (hole east path)
+                                    (labyrinth3 south path))
+                        (labyrinth3 (hole east path)
+                                    (hole south path)
+                                    (labyrinth4 west path))
+                        (labyrinth4 (hole north path)
+                                    (hole south path)
+                                    (minotaur-lair south path))
+                        (minotaur-lair (forest-trail up secret-passage))))
 
 ; This function describes a specific direction of a path from one location.
 (defun describe-path (edge)
@@ -50,18 +76,14 @@
   (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 
 ; This parameter defines the object to this game.
-(defparameter *objects* '(whiskey bucket starpiece1 frog starpiece2 chain cake starpiece3))
+(defparameter *objects* '(sword chain frog torch triforce-piece1 triforce-piece2 triforce-piece3 triforce-piece4 triforce))
 
 ; This parameter gives where the objects are in the game.
-(defparameter *object-locations* '((whiskey living-room)
-                                   (bucket living-room)
-                                   (starpiece1 living-room)
-                                   (chain garden)
-                                   (frog garden)
-                                   (starpiece2 garden)
-                                   (cake kitchen)
-                                   (starpiece3 kitchen)
-                                   (starpiece4 attic)))
+(defparameter *object-locations* '((sword house)
+                                   (torch dark-cave)
+                                   ;(defaultitem2 area3)
+                                   ;(defaultitem3 area4)
+                                   ))
 
 ; This function lists all the object to a specific location.
 (defun objects-at (loc objs obj-loc)
@@ -72,11 +94,11 @@
 ; This function describes the objects in a specific location.
 (defun describe-objects (loc objs obj-loc)
    (labels ((describe-obj (obj)
-                `(you see a ,obj on the floor.)))
+                `(you see a ,obj here.)))
       (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
 
 ; This parameter gives the starting location for this game.
-(defparameter *location* 'living-room)
+(defparameter *location* 'house)
 
 ; This function allows users to see where they are in this wizard's world.
 (defun look ()
@@ -211,7 +233,7 @@
    (t "Location already exists")))
 
 ; Adding a new location in to test                                     
-(new-location bedroom You are now in the bedroom. Every wizard needs his sleep.)
+;(new-location bedroom You are now in the bedroom. Every wizard needs his sleep.)
 (new-location outside You have jumped out the window and died. The end.)
 
 ; New implementation of new-path macro
@@ -243,7 +265,7 @@
                  (cdr (assoc ',origin *edges*)) :test 'equal)))))
 
 ; Add paths to the new location
-(new-path living-room bedroom east door west)
+;(new-path home bedroom east door west)
 (new-path attic outside outside window "unable")
 
 ; Macro to run different commands inside the game-repl
@@ -257,41 +279,170 @@
             '(i cant ,command like that.)))
           (pushnew ',command *allowed-commands*)))
 
-; Sets the parameter for the welded chain object
-(defparameter *chain-welded* nil)
+;the parameters give the combined object after the collected pieces are combined into one object.
+(defparameter *half-triforce* nil)
+(defparameter *three-quarters-triforce* nil)
+(defparameter *triforce* nil)
+(defparameter *horcrux-sword* nil)
+(defparameter *He-Man-Sword* nil)
+(defparameter *lightsaber* nil)
 
-; Uses the game-action macro to weld the chain and the bucket.
-(game-action weld chain bucket attic
-             (if (and (have 'bucket) (not *chain-welded*))
-                 (progn (setf *chain-welded* 't)
-                        '(the chain is now securely welded to the bucket.))
-               '(you do not have a bucket.)))
+;The combine-half is the first macro action for combining the first half of the triforce, using triforce-piece1 and triforce-piece2
+(game-action combine-half triforce-piece1 triforce-piece2 Blood-Island
+  (if(not(have 'triforce-piece1))
+      '(you do not have both the triforce-piece1 and triforce-piece2)
+    (if (and(have 'triforce-piece2)(not *half-triforce*))
+    (progn 
+      (setf *half-triforce* 't)
+      (new-object half-triforce Blood-Island)
+      (setf *objects*
+            (remove 'triforce-piece1 *objects*))
+      (setf *objects*
+            (remove 'triforce-piece2 *objects*))
+      (pickup 'half-triforce)
+      '(the half-triforce has been made.))
+    '(you do not have the triforce-piece2.))))
 
-; Sets the combined piece using the pieces from the previous assignment
-(defparameter *star-power* nil)
+;The combine-three-quarters is the second macro action for combining the first three pieces of the treasurekey, 
+; using half-triforce and triforce-piece3
+(game-action combine-three-quarters half-triforce triforce-piece3 Skeleton-Island
+  (if(not(have 'half-triforce))
+      '(you do not have both the half-triforce and triforce-piece3)
+    (if (and(have 'triforce-piece3)(not *three-quarters-triforce*))
+    (progn 
+      (setf *three-quarters-triforce* 't)
+      (new-object three-quarters-triforce Skeleton-Island)
+      (setf *objects*
+            (remove 'half-triforce *objects*))
+      (setf *objects*
+            (remove 'triforce-piece3 *objects*))
+      (pickup 'three-quarters-triforce)
+      '(the three-quarters-triforce has been made.))
+    '(you do not have the triforce-piece3.))))
 
-; Uses the game-action macro to combine the star pieces into one object
-(game-action power starpiece1 starpiece2 attic
-             (if (and (have 'starpiece1) (not *star-power*))
-                 (progn (setf *star-power* 't)
-                        '(the star has been formed and is giving full power.))
-               '(you do not have all the pieces to the star.)))
+;The combine is the third macro action for combining the first three pieces of the treasurekey, 
+; using three-quarters-triforce and triforce-piece4
+(game-action combine three-quarters-triforce triforce-piece4 Mysterious-Island
+  (if(not(have 'three-quarters-triforce))
+      '(you do not have both the three-quarters-triforce and triforce-piece4)
+    (if (and(have 'triforce-piece4)(not *triforce*))
+    (progn 
+      (setf *triforce* 't)
+      (new-object triforce Mysterious-Island)
+      (setf *objects*
+            (remove 'three-quarters-triforce *objects*))
+      (setf *objects*
+            (remove 'triforce-piece4 *objects*))
+      (pickup 'triforce)
+      '(the triforce has been made.))
+    '(you do not have the triforce-piece4.))))
+   
+;power-to-save macro combines the sword and the cross into one object, the horcrux-sword.
+(game-action power-to-save sword cross Rum-Island
+  (if(not(have 'sword))
+      '(you do not have both the sword and the cross)
+    (if (and(have 'cross)(not *horcrux-sword*))
+    (progn 
+      (setf *horcrux-sword* 't)
+      (new-object horcrux-sword Rum-Island)
+      (setf *objects*
+            (remove 'sword *objects*))
+      (setf *objects*
+            (remove 'cross *objects*))
+      (pickup 'horcrux-sword)
+      '(the horcrux-sword has been made. you can now defeat Bloody Mary.))
+    '(you do not have the cross.))))
 
-; Creates the parameter for the filled bucket object
-(defparameter *bucket-filled* nil)
+;i-have-the-power combines the horcrux sword with the grayskull to form the he-man-sword.
+(game-action i-have-the-power horcrux-sword grayskull Blood-Island
+  (if(not(have 'horcrux-sword))
+      '(you do not have both the horcrux-sword and the grayskull)
+    (if (and(have 'grayskull)(not *he-man-sword*))
+    (progn 
+      (setf *he-man-sword* 't)
+      (new-object he-man-sword Blood-Island)
+      (setf *objects*
+            (remove 'horcrux-sword *objects*))
+      (setf *objects*
+            (remove 'grayskull *objects*))
+      (pickup 'he-man-sword)
+      '(the he-man-sword has been made. you can now defeat Skeletor.))
+    '(you do not have the grayskull.))))
 
-; Uses the game-action macro to fill the bucket with water from the well.
-(game-action dunk bucket well garden
-             (if *chain-welded* 
-                 (progn (setf *bucket-filled* 't)
-                        '(the bucket is now full of water))
-               '(the water level is too low to reach.)))
+;jedi-power combines the he-man-sword with the skeletor-sword to make the lightsaber
+(game-action jedi-power he-man-sword skeletor-sword Skeleton-Island
+  (if(not(have 'he-man-sword))
+      '(you do not have both the he-man-sword and the skeletor-sword)
+    (if (and(have 'skeletor-sword)(not *lightsaber*))
+    (progn 
+      (setf *lightsaber* 't)
+      (new-object lightsaber Skeleton-Island)
+      (setf *objects*
+            (remove 'he-man-sword *objects*))
+      (setf *objects*
+            (remove 'skeletor-sword *objects*))
+      (pickup 'lightsaber)
+      '(the lightsaber has been made. you can now defeat One-Eyed Willy.))
+    '(you do not have the skeletor-sword.))))
 
-; Uses the game-action macro to build a new command that determines the outcome of the game.
-(game-action splash bucket wizard living-room
-             (cond ((not *bucket-filled*) '(the bucket has nothing in it.))
-                   ((have 'frog) '(the wizard awakens and sees that you stole his frog. 
-                                   he is so upset he banishes you to the 
-                                   netherworlds- you lose! the end.))
-                   (t '(the wizard awakens from his slumber and greets you warmly. 
-                        he hands you the magic low-carb donut- you win! the end.))))
+;fight-Morgan uses the game-action macro to determine the outcome of the game, whether you can move on or not.
+(game-action fight-Morgan sword Captain-Morgan Rum-Island
+             (cond ((have 'sword) (new-object cross Rum-Island)
+                                  (new-object triforce-piece1 Rum-Island)
+                                  '(You killed Captain Morgan and find the first piece of the key
+                                  guarded by Captain Morgan. You continue 
+                                  forward in the search of the keys to unlock the treasure!))
+                  (t 
+                   (setf *location* 'pirate-lair)
+                   (setf *objects* '(sword))
+                   ;(setf 'body nil)
+                   (setf *objects-locations* '((sword pirate-lair)))
+                    '(You fought valiantly but without a sword you are no match for Captain
+                        Morgan. You lose! Try again!))))
+
+;fight-Mary uses the game-action macro to determine the outcome of the game, whether you can move on.
+(game-action fight-Mary horcrux-sword Bloody-Mary Blood-Island
+             (cond ((have 'horcrux-sword) (new-object grayskull Blood-Island)
+                                  (new-object triforce-piece2 Blood-Island)
+                                  '(You killed Bloody-Mary and find the second piece of the key
+                                  guarded by Bloody-Mary. You continue 
+                                  forward in the search of the keys to unlock the treasure!))
+                  (t 
+                   (setf *location* 'pirate-lair)
+                   (setf *objects* '(sword))
+                   ;(setf *objects-locations 'body nil)
+                   (setf *objects-locations* '((sword pirate-lair)))
+                    '(You fought valiantly but without a horcrux-sword you are no match for Bloody-Mary. You lose! Try again!))))
+
+;fight-Skeletor uses the game-action macro to determine the outcome of the game, whether you can move on or not.
+(game-action fight-Skeletor he-man-sword Skeletor Skeleton-Island
+             (cond ((have 'he-man-sword) (new-object skeletor-sword Skeleton-Island)
+                                  (new-object triforce-piece3 Skeleton-Island)
+                                  '(You killed Skeletor and find the third piece of the key
+                                  guarded by Skeletor. You continue 
+                                  forward in the search of the keys to unlock the treasure!))
+                  (t 
+                   (setf *location* 'pirate-lair)
+                   (setf *objects* '(sword))
+                   (setf *objects-locations* '((sword pirate-lair)))
+                    '(You fought valiantly but without the he-man-sword you are no match for Skeletor. You lose! Try again!))))
+
+;fight-Willy uses the game action macro to determine the outcome of the game, whether you can move on or not.
+(game-action fight-Willy lightsaber One-Eyed-Willy Mystererious-Island
+             (cond ((have 'lightsaber) (new-object triforce-piece4 Mysterious-Island)
+                                  '(You killed One-Eyed-Willy and find the fourth piece of the key
+                                  guarded by One-Eyed-Willy. You continue 
+                                  forward in the search of the keys to unlock the treasure!))
+                  (t 
+                   (setf *location* 'pirate-lair)
+                   (setf *objects* '(sword))
+                   (setf *objects-locations* '((sword pirate-lair)))
+                    '(You fought valiantly but without the lightsaber you are no match for One-Eyed-Willy. You lose! Try again!))))
+
+;unlock-treasuse uses the game action macro to determine the outcome of the game. If you have the treasurekey, you win.
+;(game-action unlock-treasure treasurekey Treasure-Chest Hawaii
+ ;            (cond ((have 'treasurekey) '(ARR!! You unlocked the Treasure-Chest. The plunder is
+  ;                                              yours! You win!))
+   ;               (t                               
+    ;                '(Sorry, you haven't collected all the pieces of the treasure-key and combine it to make the treasure-key. 
+     ;                 You should look around more to find them.))))
